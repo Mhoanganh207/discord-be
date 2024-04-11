646fc1007c88f2fd9b8f8503fb08e681fff1c44e
hoanganh <hoanganh@debian.hoanganh>
2024-04-11 17:02:47 +0700

    create Channel API

--- src/controllers/ServerController.ts
+++ src/controllers/ServerController.ts
@@ -3,17 +3,19 @@ import ServerService from "../services/ServerService";
 
 class ServerController{
 
-
+    // tạo server
     public async createServer(req: Request, res: Response){
         const newServer = await ServerService.createServer(req);
         res.json(newServer);
     }
-
+    
+    // get  server theo id
     public async getServerById(req : Request, res : Response){
         const server = await ServerService.getServerById(req);
         res.json(server)
     }
 
+    // get all server mà profile tham gia
     public async getServers(req : Request, res : Response){
         const profileId = req.body.info.profileId;
         res.json(await ServerService.getAllServer(profileId));
